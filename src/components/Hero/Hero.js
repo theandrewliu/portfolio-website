9c667c31b5caf86a7044d41a06430cea7d186855
Andrew <andrew.liu.722@gmail.com>
2022-07-15 11:54:57 -0700

    added header and hero information

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm always thinking about different ways the things around me can be improved to increase efficiency or functionality. I'm a collaborative, problem-solving full-stack engineer who enjoys the new technologies used to build valuable products for users. Through my experience working in a fast-paced startup where each day was different than the one before, being adaptable and comfortable with change was required. With this exposure to an environment that shifted constantly, I'm confident in my ability to adapt to new platforms, languages, and tools I use as a software engineer.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://www.linkedin.com/in/theandrewliu'}>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
